40a17f69e910a1951847b375f4079d4274a6bbab
Deepak-197 <soni5615@gmail.com>
2023-10-07 13:35:14 +0530

    Github Homepage

---
+++ src/components/routes/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Routes,Route} from "react-router-dom"
+import GitHubHomepage from "../github-homepage/GitHubHomepage.jsx"
+
+
+
+const AllRoutes=()=>{
+    return (
+        <Routes>
+
+            <Route path="/" element={<GitHubHomepage />} />
+        
+        </Routes>
+    )
+}
+
+export default AllRoutes
